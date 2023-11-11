836653c1815f3ec02a7dbf74568b767dd15dec90
samade747 <samad.e747@gmail.com>
2023-11-11 23:33:56 +0500

    1133 11 11 2023

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ let currentScore1 = document.getElementById('currentScore1');
 let currentScore2 = document.getElementById('currentScore2');
 let totalScore1 = document.getElementById('totalScore1');
 let totalScore2 = document.getElementById('totalScore2')
+let turnChangeMessage = document.getElementById('turnChangeMessage');
 
 let currentScoreHtml1 = document.getElementById('currentScore1');
 let currentScoreHtml2 = document.getElementById('currentScore2');
@@ -39,11 +40,11 @@ currentScore2 = 0;
 rollDice.addEventListener('click', () => {
     let numberOfDice = Math.ceil(Math.random() * 6);
 
-   diceImage.src = `./images/${numberOfDice}.png`;
+   diceImage.src = `./images/${numberOfDice}.PNG`;
 
     if (numberOfDice == 1) {
         player1Turn = !player1Turn;
-        alert(`turn change ${player1Turn ? playerName1.textcontent : playerName2.textcontent}`);
+        turnChangeMessage.textContent = `Turn : ${player1Turn ? playerName1.textContent : playerName2.textContent}`;
         currentScoreHtml1.textContent = currentScore1 = 0;
         currentScoreHtml2.textContent = currentScore2 = 0;
     } else {
