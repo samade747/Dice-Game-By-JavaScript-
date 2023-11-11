3d2987a63df847c132ec8226453df59ae6c9b74f
samade747 <samad.e747@gmail.com>
2023-11-11 10:08:56 +0500

    1008AM 11112023

--- app.js
+++ app.js
@@ -1,53 +1,60 @@
 const player1Turn = true;
-const player1 = prompt('player1');
-const player2 = prompt('player2');
 
 let playerName1 = document.getElementById('player1');
 let playerName2 = document.getElementById('player2');
 
-playerName1.textContent = player1;
-playerName2.textContent = player2;
-
 const rollDice = document.getElementById('rollDice')
 const diceImage = document.getElementsByTagName("img")[0];
 let currentScore1 = document.getElementById('currentScore1');
-let currentScore2 = document.getElementById('currentScore1');
+let currentScore2 = document.getElementById('currentScore2');
 let totalScore1 = document.getElementById('totalScore1');
 let totalScore2 = document.getElementById('totalScore2')
 
 let currentScoreHtml1 = document.getElementById('currentScore1');
 let currentScoreHtml2 = document.getElementById('currentScore2');
 
-
-totalScore1.textContent = currentScore1; 
-
-
-totalScore2.textContent = currentScore2;
-
-
 currentScore1 = 0;
 currentScore2 = 0;
+
+
+
+ totalScore1.textContent = `TotalScore: ${currentScore1}`;
+ totalScore2.textContent = `TotalScore: ${currentScore2}`;
+
+function submit1(){
+    const player1Input = document.querySelector('#player1Input');   
+    playerName1.textContent = player1Input.value || 'player 1';
+    document.getElementById('1display').style.display = 'none'
+}
+
+function submit2(){    
+    const player2Input = document.querySelector('#player2Input');    
+    playerName2.textContent = player2Input.value || 'player 2';
+    document.getElementById('2display').style.display = 'none'
+    
+}
+
 
 rollDice.addEventListener('click', () =>   {
     let numberOfDice = Math.ceil(Math.random() * 6) 
 
     diceImage.src = `./images/${numberOfDice}.png`
 
-    if(numberOfDice == 1){
-        player1Turn = !player1Turn
+if(numberOfDice === 1){
+       player1Turn = !player1Turn
         alert(`turn change ${player1Turn ? playerName1.textcontent : playerName2.textcontent}`)
-        currentScoreHtml1.textContent = currentScore1 = 0;
-
-        if(player1Turn){
-            currentScoreHtml1.textcontent = currentScore1 = 0
-        } else{
-            currentScoreHtml2.textcontent = currentScore2 = 0
-        }
+        currentScoreHtml1.textContent = currentScore1.textContent = 0;
+        currentScoreHtml2.textContent = currentScore2.textContent = 0;
+        
     } else {
-        if(player1Turn)
-          currentScoreHtml1.textcontent = currentScore1 += numberOfDice;
-        else{
-          currentScoreHtml2.textcontent = currentScore2 += numberOfDice;
+        if(player1Turn) {
+          currentScoreHtml1.textcontent = currentScore1.textContent = +currentScore1.textContent + numberOfDice;
+          totalScore1.textContent = `Total Score ${+totalScore1.textContent + numberOfDice}`;
+        //   document.getElementById('Section1').style.background = 'blue'
+        } else {
+            currentScoreHtml2.textcontent = currentScore2.textContent = +currentScore2.textContent + numberOfDice;
+            totalScore2.textContent = `Total Score ${+totalScore2.textContent + numberOfDice}`;
+            // document.getElementById('Section1').style.background = 'blue'
         }
     }
 })
